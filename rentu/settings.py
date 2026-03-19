de4d1bf1660ddffeb18951eb0677e88ee61d4899
gkipping2023 <gkipping01@gmail.com>
2026-03-19 16:24:00 -0500

    debug

--- rentu/settings.py
+++ rentu/settings.py
@@ -29,7 +29,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ['192.168.0.118'] # Local network IP for development on devices
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
